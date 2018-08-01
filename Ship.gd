e080a21e4d76a69d1c791216158eb2ab3a1cd062
jmpoholarz <jmpoholarz@gmail.com>
2018-07-31 23:34:51 -0400

    Added Bullet scene; added shooting functionality

--- Ship.gd
+++ Ship.gd
@@ -1,7 +1,10 @@
 extends RigidBody2D
 
 var MAX_SPEED = 250
+var BULLET_MAG = 250
 var rotation_angle
+
+var Bullet = load("res://Bullet.tscn")
 
 func _ready():
 	position.x = 200
@@ -19,6 +22,24 @@ func _process(delta):
 		!Input.is_action_pressed("ship_rotate_left"):
 			rotation_angle += 400 * delta
 			$Sprite.rotation_degrees += 400 * delta
+	# Shoot bullet if cooldown over
+	if Input.is_action_just_pressed('ship_shoot') && $ShootDelay.is_stopped():
+		$ShootDelay.start()
+		# Create the bullet
+		var bullet = Bullet.instance()
+		# Set direction 
+		var ship_u_vec = Vector2(sin(rotation_angle*PI/180), -cos(rotation_angle*PI/180))
+		var mag = magnitude(linear_velocity.x, linear_velocity.y)
+		# If the ship is moving, account for it in the bullet velocity calculation
+		if mag > 0:
+			bullet.linear_velocity.x = ship_u_vec.x * BULLET_MAG + linear_velocity.x
+			bullet.linear_velocity.y = ship_u_vec.y * BULLET_MAG + linear_velocity.y
+		# Otherwise use just the bullet magnitude to calculate
+		else:
+			bullet.linear_velocity.x = ship_u_vec.x * BULLET_MAG
+			bullet.linear_velocity.y = ship_u_vec.y * BULLET_MAG
+		add_child(bullet)
+		
 	pass
 
 func _integrate_forces(state):
