ea3da7ba2e6d1a0586c61cdc0345d0b6840aa0a2
jmpoholarz <jmpoholarz@gmail.com>
2018-08-01 22:35:16 -0400

    Added Asteroid scenes

--- Main.gd
+++ Main.gd
@@ -1,8 +1,6 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var Asteroid = load("res://AsteroidLarge.tscn")
 
 func _ready():
 	# Called every time the node is added to the scene.
