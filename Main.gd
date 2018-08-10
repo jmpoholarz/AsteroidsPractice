0abda303ac220656d60bf99e07d04f2f1b33d3ff
jmpoholarz <jmpoholarz@gmail.com>
2018-08-09 22:05:59 -0500

    Added more UI update functions

--- Main.gd
+++ Main.gd
@@ -1,18 +1,37 @@
 extends Node
 
 var scoreValueLabel
+var highScoreValueLabel
+var lifeValueLabel
+var levelValueLabel
 
 func _ready():
+	# Reference nodes
 	scoreValueLabel = get_node("UI/LeftDock/scoreValueLabel")
+	highScoreValueLabel = get_node("UI/RightDock/highValueLabel")
+	lifeValueLabel = get_node("UI/CenterDock/lifeLevelContainer/lifeValueLabel")
+	levelValueLabel = get_node("UI/CenterDock/lifeLevelContainer/levelLabel")
+	# Set values
 	scoreValueLabel.text = str(0)
+	highScoreValueLabel.text = str(25000)
+	lifeValueLabel.text = "x3"
+	levelValueLabel.text = str(1)
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	if get_tree().get_nodes_in_group("asteroids").size() == 0:
+		increase_level()
 
 func increase_score(amount):
-	print("increase_score called with amount:" + str(amount))
 	var score = int(scoreValueLabel.text)
 	score += amount
 	scoreValueLabel.text = str(score)
+
+func set_lives_relative(amount):
+	var lives = int(lifeValueLabel.text.erase(0,1))
+	lives += amount
+	lifeValueLabel.text = "x" + str(lives)
+	
+func increase_level():
+	var level = int(levelValueLabel.text)
+	level += 1
+	levelValueLabel.text = str(level)
