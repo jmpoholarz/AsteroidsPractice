db85d4077fd642d169b0ea48683d7079191c6116
jmpoholarz <jmpoholarz@gmail.com>
2018-09-13 00:08:17 -0400

    Finished load_scores()

--- Singletons/ScoreManager.gd
+++ Singletons/ScoreManager.gd
@@ -39,16 +39,16 @@ func printScores():
 func createDefaultScores():
 	var config = ConfigFile.new()
 	# Save scores
-	config.set_value("scores", "0", 10000)
-	config.set_value("scores", "1", 9000)
-	config.set_value("scores", "2", 8000)
-	config.set_value("scores", "3", 7000)
-	config.set_value("scores", "4", 6000)
-	config.set_value("scores", "5", 5000)
-	config.set_value("scores", "6", 4000)
-	config.set_value("scores", "7", 3000)
-	config.set_value("scores", "8", 2000)
-	config.set_value("scores", "9", 1000)
+	config.set_value("scores", "0", [10000, "AAA"])
+	config.set_value("scores", "1", [9000, "BBB"])
+	config.set_value("scores", "2", [8000, "CCC"])
+	config.set_value("scores", "3", [7000, "DDD"])
+	config.set_value("scores", "4", [6000, "EEE"])
+	config.set_value("scores", "5", [5000, "FFF"])
+	config.set_value("scores", "6", [4000, "GGG"])
+	config.set_value("scores", "7", [3000, "HHH"])
+	config.set_value("scores", "8", [2000, "III"])
+	config.set_value("scores", "9", [1000, "JJJ"])
 	# Create the new config file
 	config.save("user://highscores.cfg")
 
@@ -64,6 +64,6 @@ func load_scores():
 		load_scores()
 		return
 	# Read scores
-	#if config.has_section_key("scores", "0"):
-		#var x = config.get_value("scores", "0")
-		#print(x)
+	for section in config.get_sections():
+		for key in config.get_section_keys(section):
+			scoreArray.push_back(config.get_value(section, key))
